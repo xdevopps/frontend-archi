3fb80a01fe6c9f637a6c7339cd9b86471f8a8cc1
Maryvon MICHEL <m.michel@groupe-creative.fr>
2021-11-10 23:54:49 +0100

    fix: update package

--- libs/webcomponent-xd-design/release.config.js
+++ libs/webcomponent-xd-design/release.config.js
@@ -2,7 +2,9 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
+    ["@semantic-release/npm", {
+      "pkgRoot": "dist",
+    }],
     ["@semantic-release/github", {
       "assets": [
         {"path": "dist/*.js", "label": "Package module"}
