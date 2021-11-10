5654a75a53a748e27109bafecd313384fb63613e
Maryvon MICHEL <maryvonmichel@mac-mini-de-maryvon.home>
2021-11-10 17:27:45 +0100

    feat: new publish commit component

--- libs/webcomponent-xd-design/release.config.js
+++ libs/webcomponent-xd-design/release.config.js
@@ -2,6 +2,11 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/github'
+    ["@semantic-release/github", {
+      "assets": [
+        {"path": "dist/*.js", "label": "CSS distribution"},
+        {"path": "dist/asset.min.js", "label": "JS distribution"}
+      ]
+    }],
   ]
 }
